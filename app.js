408aae294554c0fa2617a0e7a544d1aa550faabb
eteen <eteensmash@gmail.com>
2024-09-12 18:46:37 -0700

    redicrect complete

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-  res.redirect('https://teletadevelopment.com');
+app.get("/", (req, res) => {
+  res.redirect("https://teletadevelopment.com");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
